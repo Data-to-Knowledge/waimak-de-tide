79ef0baed9d70d37ddb6274f689be8d1e334c5f3
Mike Kittridge <mullenkamp1@gmail.com>
2020-02-18 11:07:29 +1300

    added 10 sec sleep for SampleBuf to kick in

--- scripts/main.py
+++ scripts/main.py
@@ -6,11 +6,12 @@ Created on Wed Apr 25 07:49:41 2018
 
 This module runs through the sequence of other python modules for updating the Waimak unmod flow in Hydrotel
 """
+import time
 
 print('Remove the tides!')
 import waimak_detide
 
+time.sleep(10)
+
 print('Create naturalised flows')
 import waimak_unmod_flow
-
-
